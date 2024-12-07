3314f998c3d04563dfcd378dcbeffef06910b9b1
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2024-12-07 20:16:52 +0000

    final

--- pembersihanData.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql.functions import col, regexp_replace
-
-# Menghapus karakter khusus dari kolom 'title'
-df_clean = df.withColumn("title", regexp_replace(col("title"), "[^a-zA-Z0-9\\s]", ""))
-
-# Menampilkan hasil setelah pembersihan
-df_clean.show(5)
